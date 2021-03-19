dd8030837ff752be3173897398788b1f2cdd4f43
nirolfdev <80695034+nirolfdev@users.noreply.github.com>
2021-03-19 13:53:48 +0000

    mod 1 -step 6 - docker build

---
+++ docker-build.sh
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose.yml -f local.docker-compose.yml -f docker-compose.init.yml build
+docker-compose -f docker-compose.yml -f local.docker-compose.yml -f docker-compose.init.yml up
